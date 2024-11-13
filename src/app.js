74ba551adc008a08ea8170a33b551ebcb5e5d69f
Tgbn99 <telmo.nunes@live.com.pt>
2024-11-13 16:32:10 +0000

    OA-6 Added Zone routes, model and controller

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
 import {} from "dotenv/config";
 import express from "express";
 
-import zoneRoutes from "./routes/zoneRoutes"
-import virusRoutes from "./routes/virusRoutes"
-import outbreakRoutes from "./routes/outbreakRoutes"
-import countryRoutes from "./routes/countryRoutes"
-import guidelineRoutes from "./routes/guidelineRoutes"
+import {zoneRoutes} from "./routes/zoneRoutes.js"
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`OMS api app listening on ports ${port}`);
 });
 
 app.use('/api/zones', zoneRoutes);
-app.use('/api/virus', virusRoutes);
-app.use('/api/outbreaks', outbreakRoutes);
-app.use('/api/countries', countryRoutes);
-app.use('/api/guidelines', guidelineRoutes);
